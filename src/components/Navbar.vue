36e939286c0b31fba782c1e1c32210f3d2ef773e
Felix <felixgohh08@gmail.com>
2019-03-21 22:19:06 +0700

    added navbar and seed images to firebase database

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container col-sm-12">
+        <div class="row">
+
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    
+}
+</script>
+
