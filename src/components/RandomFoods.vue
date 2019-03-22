1d961aca5414c65264bc2b65bcc1a987dc80d411
Felix <felixgohh08@gmail.com>
2019-03-22 15:42:20 +0700

    added swal if player win

--- src/components/RandomFoods.vue
+++ src/components/RandomFoods.vue
@@ -11,27 +11,26 @@
     </div>
     <div class="container col-sm-12 mt-5">
       <div class="row">
-        <div class="col-sm-4">
-          <h1>{{score1}}</h1>
+        <div class="col-sm-4 d-flex justify-content-center">
+          <h1 class="score">{{score1}}</h1>
         </div>
         <div class="col-sm-4 d-flex justify-content-center">
           <button class="btn btn-warning" v-if="startButton" @click="getRandom">START GAME</button>
         </div>
-        <div class="col-sm-4">
-          <h1>{{score2}}</h1>
+        <div class="col-sm-4 d-flex justify-content-center">
+          <h1 class="score">{{score2}}</h1>
         </div>
       </div>
     </div>
     <div class="imagesFooter col-sm-12" v-if="start">
       <div class="container d-flex justify-content-around">
         <div class="row" v-for="(random,index) in randomImages" :key="index">
-          <div class="col" 
-              v-if="images[index]">
+          <div class="col" v-if="images[index]">
             <img
               @click="selectImage(random.point, index)"
               :src="random.link"
-              height="170px"
-              width="220px"
+              height="220px"
+              width="250px"
             >
           </div>
         </div>
@@ -39,13 +38,22 @@
     </div>
   </div>
 </template>
-<style>
+<style scoped>
+button {
+  padding: 30px;
+  font-size: 24px;
+}
 .imagesFooter {
   position: fixed;
   left: 0;
   bottom: 0;
   width: 100%;
   background-color: #fff;
+}
+
+.score {
+  font-family: 'Permanent Marker', cursive;
+  font-size: 70px;
 }
 </style>
 
@@ -62,14 +70,13 @@ export default {
       score1: 0,
       score2: 0,
       click: 0,
-      images: [true, true, true, true, true, true, true, true],
+      images: [true, true, true, true, true, true, true, true]
     };
   },
   mounted() {
     this.allFoodImage = this.$store.state.allFoods;
     this.catImage = this.$store.state.allCats;
-    console.log(this.$store.state.roomWhoPlay, 'ini room who play');
-    
+    console.log(this.$store.state.roomWhoPlay, "ini room who play");
   },
   methods: {
     getRandom() {
@@ -100,26 +107,48 @@ export default {
       return array;
     },
     selectImage(point, index) {
-      this.click += 1
+      this.click += 1;
       if (this.click % 2 == 0) {
-        this.score2 += point
+        this.score2 += point;
         if (this.score2 <= 0) {
-          this.score2 = 0
-        } else if (this.score2 >= 100){
-          this.score2 = 100
+          this.score2 = 0;
+        } else if (this.score2 >= 100) {
+          this.score2 = 100;
+          Swal.fire({
+            title: "Player 2 win the game.",
+            width: 600,
+            padding: "3em",
+            background: "#fff url(https://images.pexels.com/photos/949592/pexels-photo-949592.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)",
+            backdrop: `
+            rgba(0,0,123,0.4)
+            url("https://thumbs.gfycat.com/RadiantJoyousGrayling-max-1mb.gif")
+            center left
+            no-repeat
+          `
+          });
         }
       } else {
-        this.score1 += point
+        this.score1 += point;
         if (this.score1 <= 0) {
-          this.score1 = 0
-        } else if (this.score1 >= 100){
-          this.score1 = 100
+          this.score1 = 0;
+        } else if (this.score1 >= 100) {
+          this.score1 = 100;
+          Swal.fire({
+            title: "Player 1 win the game",
+            width: 600,
+            padding: "3em",
+            background: "#fff url(https://66.media.tumblr.com/b832028c117cb548614cbea10f0153dc/tumblr_mudg73OFlK1rgpyeqo1_500.gif)",
+            backdrop: `
+            rgba(0,0,123,0.4)
+            url("https://images.pexels.com/photos/949592/pexels-photo-949592.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+            center left
+            no-repeat
+          `
+          });
         }
       }
       this.images[index] = false;
       console.log(point);
-      
-      
     }
   }
 };
