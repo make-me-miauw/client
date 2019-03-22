b56da8d053ed1ceeef162e88834d287a348d8ff8
Felix <felixgohh08@gmail.com>
2019-03-22 13:24:06 +0700

    added random foods component

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,62 @@
 <template>
-  <div id="app">
+  <div>
     <Room/>
     <Login/>
+    <Foods/>
     <router-view/>
   </div>
 </template>
 
 <style>
-
 </style>
 
 <script>
-import Room from '@/components/Room.vue';
-import Login from '@/views/loginform.vue';
-import db from '@/api/firebase.js';
+import Room from "@/components/Room.vue";
+import Login from "@/views/loginform.vue";
+import Foods from "@/components/RandomFoods.vue";
+import db from "@/api/firebase.js";
 
 export default {
   components: {
     Room,
     Login,
+    Foods
   },
   beforeMount() {
-    db.collection('foods').get()
-      .then((foods) => {
-        foods.forEach((doc) => {
-          this.$store.state.allFoods.push(doc.data());
-        });
-        console.log(this.$store.state.allFoods);
-      }, (err) => {
-        console.log(err);
-      })
-      .catch((err) => {
+    db
+      .collection("foods")
+      .get()
+      .then(
+        foods => {
+          foods.forEach(doc => {
+            this.$store.state.allFoods.push(doc.data());
+          });
+          console.log(this.$store.state.allFoods);
+        },
+        err => {
+          console.log(err);
+        }
+      )
+      .catch(err => {
         console.log(err);
       }),
-    db.collection('cats').get()
-      .then((cats) => {
-        cats.forEach((doc) => {
-          this.$store.state.allCats.push(doc.data());
-          console.log(this.$store.state.allCats);
+      db
+        .collection("cats")
+        .get()
+        .then(
+          cats => {
+            cats.forEach(doc => {
+              this.$store.state.allCats.push(doc.data());
+              console.log(this.$store.state.allCats);
+            });
+          },
+          err => {
+            console.log(err);
+          }
+        )
+        .catch(err => {
+          console.log(err);
         });
-      }, (err) => {
-        console.log(err);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  },
+  }
 };
 </script>
