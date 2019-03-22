411eb0e998a7185246ae3fee1515242b703d0402
Lalu Oldi Alfitra <oldia9@gmail.com>
2019-03-22 14:04:47 +0700

    added router play

--- src/router.js
+++ src/router.js
@@ -6,5 +6,13 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
+<<<<<<< 374f80de9780ee3ca3328382d0bbbd3377904740
   routes: [],
+=======
+  routes: [{
+    path: '/play',
+    name: 'play',
+    component: () => import(/* webpackChunkName "play" */'@/components/randomFoods.vue'),
+  }],
+>>>>>>> added router play
 });
